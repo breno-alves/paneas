45f096b39838acbb64d356b89ccbee1cc9b702ae
Breno Alves <brenoalvesdev@gmail.com>
2023-07-04 09:36:01 -0300

    feat: add configuration module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { JokesModule } from './modules/jokes/jokes.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { HealthModule } from './shared/health/health.module';
+import configuration from './shared/config/app.config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({ isGlobal: true, load: [configuration] }),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
---
+++ src/shared/config/app.config.ts
@@ -0,0 +1,21 @@
+interface Config {
+  app: {
+    env: string;
+    port: number;
+  };
+  database: {
+    uri: string;
+  };
+}
+
+export default function configuration(): Config {
+  return {
+    app: {
+      env: process.env.NODE_ENV ?? 'development',
+      port: +process.env.APP_PORT ?? 3000,
+    },
+    database: {
+      uri: process.env.DATABASE_URI ?? 'mongodb://localhost:27017/jokes',
+    },
+  };
+}
